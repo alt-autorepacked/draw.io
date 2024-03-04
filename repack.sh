a6281d4f5154607c435dec0aad560037d14f9938
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-03-04 19:45:04 +0300

    init

---
+++ repack.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+COMMON_VERSION="0.4.0"
+
+epm tool eget https://raw.githubusercontent.com/alt-autorepacked/common/v$COMMON_VERSION/common.sh
+. ./common.sh
+# . ../common/common.sh
+
+_package="draw.io"
+
+arch="$(epm print info --debian-arch)"
+
+GITHUB_REPO="jgraph/drawio-desktop"
+GITHUB_SUFFIX="drawio-$arch-*.deb"
+
+download_version=$(_check_version_from_github)
+remote_version=$(_check_version_from_remote)
+
+if [ "$remote_version" != "$download_version" ]; then
+    _download_from_github
+    _add_repo_suffix
+    TAG="v$download_version"
+    _create_release
+    echo "Release created: $TAG"
+else
+    echo "No new version to release. Current version: $download_version"
+fi
+
+rm common.sh
